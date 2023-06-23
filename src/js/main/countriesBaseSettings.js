ecf51f7dc5f31a3cf5fabdc5e224da01457c50d2
DenisTunyk <98811233+DenisTunyk@users.noreply.github.com>
2023-06-23 16:18:21 +0300

    dsf

--- src/js/main/countriesBaseSettings.js
+++ src/js/main/countriesBaseSettings.js
@@ -1,6 +1,6 @@
 export const countriesItems = [
-    {srcPath: './img/1f1e6-1f1eb.png', alt: '🇦🇫', dataPath: './images/1f1e6-1f1eb.png', countryName: 'Afghanistan', countryCode: '+93'},
-    {srcPath: '/1f1e6-1f1f1.png', alt: '🇦🇱', dataPath: './images/1f1e6-1f1f1.png', countryName: 'Albania', countryCode: '+355'},
-    {srcPath: '/1f1e9-1f1ff.png', alt: '🇩🇿', dataPath: './images/1f1e9-1f1ff.png', countryName: 'Algeria', countryCode: '+213'},
-    {srcPath: '/1f1e6-1f1f8.png', alt: '🇦🇸', dataPath: './images/1f1e6-1f1f8.png', countryName: 'American Samoa', countryCode: '+1684'},
+    {srcPath: './images/1f1e6-1f1eb.png', alt: '🇦🇫', dataPath: './images/1f1e6-1f1eb.png', countryName: 'Afghanistan', countryCode: '+93'},
+    {srcPath: './images/1f1e6-1f1f1.png', alt: '🇦🇱', dataPath: './images/1f1e6-1f1f1.png', countryName: 'Albania', countryCode: '+355'},
+    {srcPath: './images/1f1e9-1f1ff.png', alt: '🇩🇿', dataPath: './images/1f1e9-1f1ff.png', countryName: 'Algeria', countryCode: '+213'},
+    {srcPath: './images/1f1e6-1f1f8.png', alt: '🇦🇸', dataPath: './images/1f1e6-1f1f8.png', countryName: 'American Samoa', countryCode: '+1684'},
 ]
